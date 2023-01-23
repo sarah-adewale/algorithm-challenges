c74a4634ba5a3370f02b11ad9ff65d596a1826df
Sarah Adewale <saraphynaadewale@gmail.com>
2023-01-23 18:49:28 +0100

    added pizza night codewars chalenge

---
+++ pizza-night.js
@@ -0,0 +1,37 @@
+/* Pizza Night? 
+It's the weekend and you and your friends can't agree on 
+what to order for dinner, so you put it to a vote. 
+
+Write a function to find the food with the highest number of votes. 
+
+Your function should take in a food object and find the food
+with the most votes. It should log the winner, along with 
+how many votes it received.  
+
+Example input: {"🐈 cats": 19, "🐕 dogs": 17} 
+Example output: The winner is 🐈 cats with 19 votes!
+*/ 
+
+const gameNightFood = {
+    "🍕 pizza": 3, 
+    "🌮 tacos": 10, 
+    "🥗 salads": 7,
+    "🍝 pasta": 5
+}
+
+function findTheWinner(obj){
+let highestVotes = 0
+let winningItem = ''
+for(let food in obj){
+    if(obj[food] > highestVotes){
+        highestVotes = obj[food]
+        winningItem = food
+    }
+   
+//    console.log(highestVotes, winningItem) 
+}
+return `The winner is ${winningItem} with ${highestVotes} votes`
+
+}
+
+console.log(findTheWinner(gameNightFood));
