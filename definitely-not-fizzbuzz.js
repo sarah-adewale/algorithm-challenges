f906d3f85a7fe57ef0d168e3bf85947e51215a79
Sarah Adewale <saraphynaadewale@gmail.com>
2023-02-19 20:15:20 +0100

    modified definetely not fizzbuzz scrimba challenge

--- definitely-not-fizzbuzz.js
+++ definitely-not-fizzbuzz.js
@@ -6,7 +6,7 @@ scrimba has 100 employees and their employee ID numbers range from 1 - 100. if t
 employee's ID number is
 
 divisible by 3 - Vacation!
-divisible by 5 - $100,000 bonue!
+divisible by 5 - $100,000 bonus!
 divisible by both 3 and 5 - jackpot! 1 million and a yatch
 not divisible by 3 and 3  or  5 :(
 
@@ -20,23 +20,37 @@ your function's output should look something like this:
 5: $100,000 bonus!
 */
 
+// function awardBonuses(num){
+//     //loop thru the values in num
+//     for(let i = 1; i <= num; i++){
+//         if(i % 3 === 0 && i % 5 === 0){
+//             console.log(`${i} - jackpot! 1 million and a yatch`) 
+//         }else if(i % 3 !== 0 && i % 5 !== 0){
+//             console.log(`${i} - :(`) 
+//         }else if(i % 3 === 0){
+//             console.log(`${i} - Vacation!`) 
+//         }else if(i % 5 === 0){
+//             console.log(`${i} - $100,000 bonus!`) 
+//         }
+//         // else{
+//         //     console.log(`${i}`) 
+//         // }
+//     }
+//     //a conditional to do validations; 
+// }
+
 function awardBonuses(num){
-    //loop thru the values in num
     for(let i = 1; i <= num; i++){
         if(i % 3 === 0 && i % 5 === 0){
-            console.log(`${i} - jackpot! 1 million and a yatch`) 
+            console.log('jackpot! 1 million and a yatch')
         }else if(i % 3 !== 0 && i % 5 !== 0){
-            console.log(`${i} - :(`) 
+            console.log(':(')
         }else if(i % 3 === 0){
-            console.log(`${i} - Vacation!`) 
+            console.log('Vacation!')
         }else if(i % 5 === 0){
-            console.log(`${i} - $100,000 bonus!`) 
+            console.log('$100,000 bonus!')
         }
-        // else{
-        //     console.log(`${i}`) 
-        // }
     }
-    //a conditional to do validations; 
 }
 
 console.log(awardBonuses(100))
