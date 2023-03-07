c051b82c44bd93a56f5e5ef30fdc2bf0c4bc3b2b
Sarah Adewale <saraphynaadewale@gmail.com>
2023-03-07 18:20:43 +0100

    added total savory scrimba challenge

---
+++ total-savory-items.js
@@ -0,0 +1,60 @@
+
+const shoppingCart = [
+    {
+        item: "🍭",
+        price: 2.99,
+        type: "sweet",
+        onSale: true
+    },
+    {
+        item: "🍫",
+        price: 1.99, 
+        type: "sweet",
+        onSale: true
+    },
+    {
+        item: "🥫",
+        price: 1.99, 
+        type: "savory",
+        onSale: false
+    },
+    {
+        item: "🍬",
+        price: .89, 
+        type: "sweet",
+        onSale: false
+    },
+    {
+        item: "🥦",
+        price: 3.99,
+        type: "savory",
+        onSale: false
+    }, 
+    {
+        item: "🍖",
+        price: 3.99, 
+        type: "savory",
+        onSale: true
+    }, 
+]
+
+/*
+Use reduce() and only reduce() to calculate and return 
+the total cost of only the savory
+items in the shopping cart.
+
+Expected output: 9.97  
+*/
+
+function totalSavory(arr){
+    return arr.reduce((acc, c) => {
+        if(c.type === 'savory'){
+             return acc+=c.price
+        }else{
+            return acc
+        }
+        
+    }, 0)
+}
+
+console.log(totalSavory(shoppingCart));
